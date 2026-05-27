dadecc53124de80acf4318805a53461788cb98f9
Dan-Kira <daniel_t_mendes@estudante.sesisenai.org.br>
2026-05-27 19:52:57 -0300

    Create SpawnsData.cs

---
+++ SpawnsData.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Novo spawn", menuName = "Spawns")]
+public class SpawnsData : ScriptableObject
+{
+    public float positionX;
+    public float positionY;
+    public float positionZ;
+}
